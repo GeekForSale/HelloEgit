e67f0101bc36ade64c5a61e592f8e49e05ffc0d9
Jason Webster <geekforsale@gmail.com>
2011-08-23 20:52:29 -0700

    Initial commit of HelloGIT

---
+++ HelloGIT/src/geekforsale/HelloGIT.java
@@ -0,0 +1,14 @@
+package geekforsale;
+
+public class HelloGIT {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git!");
+
+	}
+
+}
