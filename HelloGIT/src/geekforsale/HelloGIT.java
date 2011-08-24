4430ba9d3d3de3a8d44356c00af0ef26425654c1
Jason Webster <geekforsale@gmail.com>
2011-08-23 22:20:47 -0700

    What's going on here....

--- HelloGIT/src/geekforsale/HelloGIT.java
+++ HelloGIT/src/geekforsale/HelloGIT.java
@@ -2,15 +2,19 @@ package geekforsale;
 
 public class HelloGIT {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!!!");
 		//We have made some changes to Git!
 		System.out.println("Hello Jason!");
 
+		// More bang.
+		for(int i=0;i<100;i++){
+			if(i%10==0)
+				System.out.println();
+			System.out.print("bang!");
+		}
+		
 	}
 
 }
