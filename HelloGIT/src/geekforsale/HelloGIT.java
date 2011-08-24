20ce6e723031c3e0dcdbd2b69ab2502e2b57eb35
Jason Webster <geekforsale@gmail.com>
2011-08-23 20:54:33 -0700

    Added answer to initial message.

--- HelloGIT/src/geekforsale/HelloGIT.java
+++ HelloGIT/src/geekforsale/HelloGIT.java
@@ -8,6 +8,8 @@ public class HelloGIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
+		//We have made some changes to Git!
+		System.out.println("Hello Jason!");
 
 	}
 
